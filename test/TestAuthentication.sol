1536ca007be025df91ee7e6be5887871ffacde54
John McCarthy <post.john.mccarthy@gmail.com>
2018-03-20 11:04:16 -0600

    add tests for Authentication.sol, helper contract for error testing

--- test/TestAuthentication.sol
+++ test/TestAuthentication.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.2;
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/Authentication.sol";
+import "./helpers/ErrorProxy.sol";
 
 contract TestAuthentication {
 
@@ -13,7 +14,63 @@ contract TestAuthentication {
 
     bytes32 expected = 'testuser';
 
-    Assert.equal(authentication.login(), expected, "It should sign up and log in a user.");
+    Assert.equal(authentication.login(), expected, "It signs up and logs in a user.");
+  }
+
+  function testUserCanUpdateName() {
+    testUserCanSignUpAndLogin();
+
+    Authentication authentication = Authentication(DeployedAddresses.Authentication());
+
+    authentication.update('new_name');
+
+    bytes32 expected = 'new_name';
+
+    Assert.equal(authentication.login(), expected, "It updates a user.");
+  }
+
+  function testLoginFailure() {
+    Authentication authentication = new Authentication();
+    ErrorProxy errorProxy = new ErrorProxy(address(authentication));
+
+    Authentication(address(errorProxy)).login();
+
+    bool r = errorProxy.execute.gas(200000)();
+
+    Assert.isFalse(r, "Only existings users can log in.");
+  }
+
+  function testSignUpFailure() {
+    Authentication authentication = new Authentication();
+    ErrorProxy errorProxy = new ErrorProxy(address(authentication));
+
+    Authentication(address(errorProxy)).signup('');
+
+    bool r = errorProxy.execute.gas(200000)();
+
+    Assert.isFalse(r, "Cannot sign up with invalid name.");
+  }
+
+  function testUpdateWithInvalidName() {
+    Authentication authentication = new Authentication();
+    ErrorProxy errorProxy = new ErrorProxy(address(authentication));
+
+    Authentication(address(errorProxy)).update('');
+
+    bool r = errorProxy.execute.gas(200000)();
+
+    Assert.isFalse(r, "Cannot update user with invalid name.");
+  }
+
+  function testUpdateWithInvalidUser() {
+    Authentication authentication = new Authentication();
+    ErrorProxy errorProxy = new ErrorProxy(address(authentication));
+
+    Authentication(address(errorProxy)).update('new_name');
+
+    bool r = errorProxy.execute.gas(200000)();
+
+    Assert.isFalse(r, "Cannot update non-existent user.");
   }
 
 }
